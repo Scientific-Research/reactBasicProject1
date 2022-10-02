b86b939f8a9f5e91d44fc9f2247f6b6a18cc278c
Maximilian <scientificresearch309@gmail.com>
2022-10-03 00:28:34 +0200

    randomized array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import "./App.scss";
-import germanNouns from "./data/germanNouns.json";
+import _germanNouns from "./data/germanNouns.json";
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+// const germanNouns = shuffle(_germanNouns).filter(m=>m.article ==="das");
+const germanNouns = shuffle(_germanNouns);
+
+
 function App() {
   return (
     <div className="App">
