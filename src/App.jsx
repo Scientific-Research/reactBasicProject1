79645dfd745142717e18256f6bab607320fafbc2
Maximilian <scientificresearch309@gmail.com>
2022-10-03 01:26:48 +0200

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import "./App.scss";
 import _germanNouns from "./data/germanNouns.json";
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex != 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
@@ -21,7 +23,6 @@ function shuffle(array) {
 
 // const germanNouns = shuffle(_germanNouns).filter(m=>m.article ==="das");
 const germanNouns = shuffle(_germanNouns);
-
 
 function App() {
   return (
@@ -33,8 +34,13 @@ function App() {
         {germanNouns.map((germanNoun) => {
           return (
             <div className="germanNoun">
-              <div className="singular">{germanNoun.article} {germanNoun.singular}</div>
-              <div className="plural">{germanNoun.plural} </div>
+              <img src="/public/images/cardback.jpg" alt="cardback.jpg" />
+              <div>
+                <div className="singular">
+                  {germanNoun.article} {germanNoun.singular}
+                </div>
+                <div className="plural">{germanNoun.plural} </div>
+              </div>
             </div>
           );
         })}
