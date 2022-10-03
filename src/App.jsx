4b9710dc724c2a93112bc2b32b36f6b3a38eea3f
Maximilian <scientificresearch309@gmail.com>
2022-10-03 02:21:18 +0200

    german nouns showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,12 @@
 import "./App.scss";
-import _germanNouns from "./data/germanNouns.json";
+import { GermanNouns } from "./components/GermanNouns";
 
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-
-// const germanNouns = shuffle(_germanNouns).filter(m=>m.article ==="das");
-const germanNouns = shuffle(_germanNouns);
 
 function App() {
   return (
     <div className="App">
       <h1>Test Site</h1>
-      <h2>German Nouns</h2>
-      <p>There are here {germanNouns.length} German nouns.</p>
-      <div className="germanNouns">
-        {germanNouns.map((germanNoun) => {
-          return (
-            <div className="germanNoun">
-              <img src="images/cardback.jpg" alt="" />
-              <div>
-                <div className="singular">
-                  {germanNoun.article} {germanNoun.singular}
-                </div>
-                <div className="plural">{germanNoun.plural} </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <GermanNouns/>  
     </div>
   );
 }
---
+++ src/components/GermanNouns.jsx
@@ -0,0 +1,47 @@
+import _germanNouns from "../data/germanNouns.json";
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+// const germanNouns = shuffle(_germanNouns).filter(m=>m.article ==="das");
+const germanNouns = shuffle(_germanNouns);
+export const GermanNouns = ({ name = "(empty)" }) => {
+  return (
+    <>
+      <h2>German Nouns</h2>
+      <p>There are here {germanNouns.length} German nouns.</p>
+      <div className="germanNouns">
+        {germanNouns.map((germanNoun) => {
+          return (
+            <div className="germanNoun">
+              <img src="images/cardback.jpg" alt="" />
+              <div>
+                <div className="singular">
+                  {germanNoun.article} {germanNoun.singular}
+                </div>
+                <div className="plural">{germanNoun.plural} </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
